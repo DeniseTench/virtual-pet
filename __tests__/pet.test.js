1ea485d612ac217beac203468d4766882f8218bd
Denise Tench <denise.tench@hotmail.co.uk>
2019-11-06 23:20:27 +0000

    Add files via upload

--- __tests__/pet.test.js
+++ __tests__/pet.test.js
@@ -1,8 +1,45 @@
-function Pet() {}
+const Pet = require('../src/pet');
 
-module.exports = Pet;
 describe('constructor', () => {
     it('returns an object', () => {
       expect(new Pet('Fido')).toBeInstanceOf(Object);
     });
   });
+
+  describe('constructor', () => {  
+    it('has a initial age of 0', () => {
+      const pet = new Pet('Fido');
+  
+      expect(pet.age).toEqual(0);
+    });
+  });
+
+  describe('growUp', () => {
+    it('increments the age by 1', () => {
+      const pet = new Pet('Fido');
+  
+      pet.growUp();
+  
+      expect(pet.age).toEqual(1);
+    });
+
+    it('increments hunger by 5', () => {
+      const pet = new Pet('Fido');
+
+      expect(pet.hunger).toEqual(0);
+    });
+  
+  it('decrements fitness by 3', () => {
+    const pet = new Pet('Fido');
+
+    expect(pet.fitness).toEqual(10);
+  });
+});
+
+describe ('walk', () => {
+  it('increments fitness by 4', () => {
+    const pet = new Pet('Fido');
+
+    expect(pet.fitness).toEqual(10);
+  });
+});
--- src/pet.js
+++ src/pet.js
@@ -1,3 +1,23 @@
-function Pet() {}
+function Pet(name) {
+   this.name = name;
+   this.age = 0; 
+   this.hunger = 0;
+   this.fitness = 10; 
+   this.growUp = function() {
+      this.age += 1;
+      this.hunger += 5; 
+      this.fitness -= 3;
+   };
+   this.walk = function() {
+      this.fitness += 4;
+      if(fitness >= 11) {
+         console.log(10);  
+        } else {
+          console.log();
+        }      
+   };
+}
 
 module.exports = Pet;
+
+//A pet's fitness level should never go above 10, so if the fitness level is 9, walk should increase fitness to 10
